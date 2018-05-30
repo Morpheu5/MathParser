41d10c4fbc2e1bba57e8513bc2a774254718080f
Andrea Franceschini <andrea.franceschini@gmail.com>
2018-05-30 15:31:49 +0100

    Fix gh-pages deployment.

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,20 @@
 // vue.config.js
 module.exports = {
-    configureWebpack: {
-      module: {
-        rules: [
-          {
-            test: /\.ne$/,
-            use: [
-              'nearley-loader',
-            ],
-          },
-        ]
-      }
+  configureWebpack: (config) => {
+    let c = {}
+    c.module = {
+      rules: [
+        { test: /\.ne$/,
+          use: [ 'nearley-loader' ],
+        },
+      ]
     }
-  }
+    // if (process.env.NODE_ENV === 'production') {
+    //   c.module.build = {
+    //     assetsPublicPath: '/MathParser'
+    //   }
+    // }
+    return c
+  },
+  baseUrl: process.env.NODE_ENV === 'production' ? '/MathParser' : undefined
+}
